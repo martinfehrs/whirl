d1a43be7749cb57143791c29a7273545aa8abcbc
Martin Kalbfuß <ma.kalbfuss@web.de>
2018-07-22 15:31:24 +0200

    resloving name conflict. Introduction of nested namespaces for predefined tokens and sets

--- examples/sequential.cpp
+++ examples/sequential.cpp
@@ -2,12 +2,15 @@
 #include <fstream>
 #include <iostream>
 
+
 auto read_temperature(std::istream& ins, LL1::code_position& pos)
 {
-    auto has_sign = LL1::ignore_if(ins, pos, '-');
-    auto val = LL1::expect(ins, pos, LL1::digit) - '0';
+    using LL1::sets::digit;
 
-    while(LL1::is(ins, LL1::digit))
+    auto has_sign = LL1::ignore_if(ins, pos, '-');
+    auto val = LL1::expect(ins, pos, digit) - '0';
+
+    while(LL1::is(ins, digit))
         val = val * 10 + LL1::read(ins, pos) - '0';
 
     return val * (has_sign ? -1 : 1);
@@ -15,12 +18,14 @@ auto read_temperature(std::istream& ins, LL1::code_position& pos)
 
 auto read_temperatures(std::istream& ins, LL1::code_position& pos)
 {
+    using LL1::sets::space;
+
     std::vector<int> temperatures;
 
     while(LL1::is_not(ins, EOF))
     {
         temperatures.push_back(read_temperature(ins, pos));
-        LL1::ignore_while(ins, pos, LL1::space);
+        LL1::ignore_while(ins, pos, space);
     }
 
     return temperatures;
